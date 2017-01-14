f94ee776585bfc1b8a635c0164875d791b948cf2
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-01-14 21:29:10 +0100

    Implemented second tab

--- src/main/java/pl/poznan/put/fc/gi/frontend/controllers/RootController.java
+++ src/main/java/pl/poznan/put/fc/gi/frontend/controllers/RootController.java
@@ -1,16 +1,18 @@
 package pl.poznan.put.fc.gi.frontend.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.SplitPane;
+import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import pl.poznan.put.fc.gi.frontend.utils.WordsCloudGeneratorUtil;
 import pl.poznan.put.fc.gi.frontend.views.SingleWordsCloudView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author Kamil Walkowiak
@@ -18,16 +20,46 @@ import java.io.IOException;
 public class RootController {
     @FXML
     private Tab summaryTab;
+    @FXML
+    private ArrayList<ImageView> wordsCloudImageViews;
+
+    private Image summaryWordsCloudImage;
+    private Image miniWordsCloudImage;
 
     @FXML
     private void initialize() throws IOException {
-        Image wordsCloudImage = WordsCloudGeneratorUtil.generateWordsCloudImage(15);
-        SingleWordsCloudView singleWordsCloudView = new SingleWordsCloudView(wordsCloudImage);
+        summaryWordsCloudImage = WordsCloudGeneratorUtil.getInstance().generateWordsCloudImage(15);
+        miniWordsCloudImage = WordsCloudGeneratorUtil.getInstance().generateWordsCloudImage(5);
 
+        SingleWordsCloudView singleWordsCloudView = new SingleWordsCloudView(summaryWordsCloudImage);
         BorderPane borderPane = (BorderPane)summaryTab.getContent();
         HBox layout = singleWordsCloudView.getLayout();
         borderPane.setCenter(layout);
         layout.prefWidthProperty().bind(borderPane.widthProperty());
         layout.prefHeightProperty().bind(borderPane.heightProperty());
+
+        setupImageViews();
+    }
+
+    private void setupImageViews() {
+        for(ImageView imageView: wordsCloudImageViews) {
+            imageView.setImage(miniWordsCloudImage);
+            imageView.setOnMouseClicked(event -> {
+                try {
+                    showYearDetails();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+
+    private void showYearDetails() throws IOException {
+        Stage stage = new Stage();
+        stage.setMaximized(true);
+        stage.setTitle("Podsumowanie roku");
+        SingleWordsCloudView singleWordsCloudView = new SingleWordsCloudView(summaryWordsCloudImage);
+        stage.setScene(new Scene(singleWordsCloudView.getLayout()));
+        stage.show();
     }
 }
--- src/main/java/pl/poznan/put/fc/gi/frontend/utils/WordsCloudGeneratorUtil.java
+++ src/main/java/pl/poznan/put/fc/gi/frontend/utils/WordsCloudGeneratorUtil.java
@@ -3,32 +3,45 @@ package pl.poznan.put.fc.gi.frontend.utils;
 import com.kennycason.kumo.CollisionMode;
 import com.kennycason.kumo.WordCloud;
 import com.kennycason.kumo.WordFrequency;
-import com.kennycason.kumo.bg.RectangleBackground;
 import com.kennycason.kumo.font.scale.LinearFontScalar;
 import com.kennycason.kumo.nlp.FrequencyAnalyzer;
 import com.kennycason.kumo.palette.ColorPalette;
+import com.kennycason.kumo.wordstart.CenterWordStart;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 
 import java.awt.*;
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * @author Kamil Walkowiak
  */
-public abstract class WordsCloudGeneratorUtil {
-    public static Image generateWordsCloudImage(int wordsCount) {
-        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
-        frequencyAnalyzer.setWordFrequenciesToReturn(wordsCount);
+public class WordsCloudGeneratorUtil {
+    private static WordsCloudGeneratorUtil instance;
+    private List<WordFrequency> wordFrequencies;
 
-        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load(Arrays.asList("Buenos Aires", "Buenos Aires", "Córdoba", "La Plata"));
-        final Dimension dimension = new Dimension(700, 700);
-        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
-        wordCloud.setPadding(2);
+    private WordsCloudGeneratorUtil() throws IOException {
+        FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        wordFrequencies = frequencyAnalyzer.load(ClassLoader.class.getResourceAsStream("/showcase/ExampleData.txt"));
+    }
+
+    public static WordsCloudGeneratorUtil getInstance() throws IOException {
+        if(instance == null) {
+            instance = new WordsCloudGeneratorUtil();
+        }
+
+        return instance;
+    }
+
+    public Image generateWordsCloudImage(int wordsCount) throws IOException {
+        final Dimension dimension = new Dimension(750, 750);
+        WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+        wordCloud.setPadding(10);
+        wordCloud.setWordStartStrategy(new CenterWordStart());
         wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
-        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
-        wordCloud.build(wordFrequencies);
+        wordCloud.setFontScalar(new LinearFontScalar(10, 100 + (15 - wordsCount)*5));
+        wordCloud.build(wordFrequencies.subList(0, wordsCount));
         return SwingFXUtils.toFXImage(wordCloud.getBufferedImage(), null);
     }
 }
